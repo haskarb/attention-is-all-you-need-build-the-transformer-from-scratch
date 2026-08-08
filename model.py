050102c0e7bad91faf50867e06b337d34607d4c9
Bhaskar Dhariyal <20501023+haskarb@users.noreply.github.com>
2026-08-08 06:48:26 +0100

    Step 24: transpose_heads_before_sequence

--- model.py
+++ model.py
@@ -247,8 +247,12 @@ def split_last_dim_into_heads(tensor, num_heads):
     d_model = d_model // num_heads
     return tensor.reshape(B, L, num_heads, d_model)
 
-# Step 24 - transpose_heads_before_sequence (not yet solved)
-# TODO: implement
+# Step 24 - transpose_heads_before_sequence
+import torch
+
+def transpose_heads_before_sequence(split_tensor):
+    
+    return split_tensor.transpose(1, 2)
 
 # Step 25 - merge_heads_back_to_model_dim (not yet solved)
 # TODO: implement
